44b9e99634b98d6c99219d0cc5f0909fb9768544
nicolas.charlot <nicolas.charlot@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-08-20 16:37:23 +0000

    Partial d'affichage du fil d'Ariane : prise en compte d'une option facultative `separator` (par défaut `&raquo;`)
    git-svn-id: http://svn.symfony-project.com/plugins/isicsBreadcrumbsPlugin@21279 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/isicsBreadcrumbs/templates/_show.php
+++ modules/isicsBreadcrumbs/templates/_show.php
@@ -1,8 +1,10 @@
 <?php $last_key = count($items) - 1 ?>
 
+<?php !isset($separator) and $separator = '&raquo;' ?>
+
 <?php foreach ($items as $key => $item): ?>
   <?php if ($key < $last_key): ?>
-    <?php echo link_to($item->getText(), $item->getUri(ESC_RAW)) ?> &raquo;
+    <?php echo link_to($item->getText(), $item->getUri(ESC_RAW)) ?> <?php echo $separator ?>
   <?php else: ?>
     <strong><?php echo $item->getText() ?></strong>
   <?php endif ?>
