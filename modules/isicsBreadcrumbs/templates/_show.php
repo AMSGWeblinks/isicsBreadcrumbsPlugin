f7d274267ca05ade8b1574aa265fd347407e67d2
Pawel Wilk <pawel@wilk.pro>
2011-11-03 16:04:24 +0100

    _shop php - warning in test fixed

--- modules/isicsBreadcrumbs/templates/_show.php
+++ modules/isicsBreadcrumbs/templates/_show.php
@@ -4,12 +4,14 @@
 
 <?php foreach ($items as $key => $item): ?>
   <?php if ($key < $last_key): ?>
-    <?php echo link_to(
-      $item->getText(),
-      $item->getUri(ESC_RAW),
-      $item->getOptions() instanceOf sfOutputEscaperArrayDecorator ? $item->getOptions()->getRawValue() : $item->getOptions()
-    ) ?>  
-    <?php echo $separator ?>
+    <?php if ($item->getUri()): ?>
+      <?php echo link_to(
+        $item->getText(),
+        $item->getUri() instanceOf sfOutputEscaperArrayDecorator ? $item->getUri()->getRawValue() : $item->getUri(),
+        $item->getOptions() instanceOf sfOutputEscaperArrayDecorator ? $item->getOptions()->getRawValue() : $item->getOptions()
+      ) ?>
+      <?php echo $separator ?>
+    <?php endif ?>
   <?php else: ?>
     <strong><?php echo $item->getText() ?></strong>
   <?php endif ?>
